013bb18e40ca3a8c0efefadce5cef64d95fecc86
tixxff <awatif5166@gmail.com>
2019-09-22 10:46:14 +0700

    add maha-lap add test

--- maha-lap.js
+++ maha-lap.js
@@ -1,6 +1,15 @@
 class MahaLap {
   static tamnaiWithDay(day) {
-    return 2;
+    switch (true) {
+      case day === "วันจันทร์":
+        return 2;
+
+      case day === "วันอังคาร":
+        return 3;
+
+      case day === "วันพุธ":
+        return 4;
+    }
   }
 }
 module.exports = MahaLap;
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -6,7 +6,12 @@ describe("Test Maha-Lab", () => {
   });
 
   test("Test Tamnai with day", () => {
-    const result = MahaLap.tamnaiWithDay("วันอาทิตย์");
-    expect(result).toEqual(1);
+    const result = MahaLap.tamnaiWithDay("วันอังคาร");
+    expect(result).toEqual(3);
+  });
+
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("วันพุธ");
+    expect(result).toEqual(4);
   });
 });
